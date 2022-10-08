17b3e220d6e3ac7ba78ad810f17a01bb6c7ef73f
Bredis <77401763+Bredis79@users.noreply.github.com>
2022-10-08 05:12:58 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,19 @@
+$(function(){
+
+    $('.axis').animate({ width: '73.5%' }, 200);
+
+
+// this piece of code i found on stackoverflow    
+// Animate the element's value from 0% to 110%:
+$({someValue: 0}).animate({someValue: 185}, {
+	duration: 3000,
+	easing:'swing', // can be anything
+	step: function() { // called on every step
+		// Update the element's text with rounded-up value:
+		$('#value').text(Math.ceil(this.someValue));
+	}
+});
+
+
+
+})
