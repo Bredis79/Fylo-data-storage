61f9db6931919d04ceb73c901ee6156b74f811cf
Bredis <77401763+Bredis79@users.noreply.github.com>
2022-10-13 16:25:28 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ $(function(){
     $('.axis').animate({ width: '73.5%' }, 200);
 
 
-// this piece of code i found on stackoverflow    
+// this piece of code is from stackoverflow    
 // Animate the element's value from 0% to value you will set:
 $({someValue: 0}).animate({someValue: 185}, {
 	duration: 3000,
